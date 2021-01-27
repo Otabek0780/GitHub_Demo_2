caf517f7f8398009ed8d56df825d84acd90f0f0d
Otabek0780 <mrdunacdtm@gmail.com>
2021-01-27 22:15:11 +0900

    Initial Commit

---
+++ GitHub_Demo_2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GitHub_Demo_2
+//
+//  Created by kim jong moon on 27/01/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
